a7f42c86e5b665ec3aa31d1f077ee0b232cc4e7b
Jan <jan-luca.net@t-online.de>
2020-12-04 15:22:23 +0100

    Fix Dropmenü

--- COM/main_gui.py
+++ COM/main_gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import threading
 import verbindungsTest
-#tupel to string imports
+# tupel to string imports
 import functools
 import operator
 
@@ -11,16 +11,18 @@ main.config(bg="grey")
 main.title("Main Interface")
 main.resizable(False, False)
 
+
 def convertTuple(tup):
     str = functools.reduce(operator.add, (tup))
     return str
+
 
 def hoeheEinstellen():
     hoehe = 0
     write(selectedHoehe.get())
     if (selectedHoehe.get() == "Höhe 1"):
         hoehe = 1
-    elif(selectedHoehe.get() == "Höhe 2"):
+    elif (selectedHoehe.get() == "Höhe 2"):
         hoehe = 2
     elif (selectedHoehe.get() == "Höhe 3"):
         hoehe = 3
@@ -33,12 +35,13 @@ def geschwEinstellen():
     write(selectedGeschw.get())
     if (selectedGeschw.get() == "Geschwindigkeit 1"):
         geschw = 1
-    elif(selectedGeschw.get() == "Geschwindigkeit 2"):
+    elif (selectedGeschw.get() == "Geschwindigkeit 2"):
         geschw = 2
     elif (selectedGeschw.get() == "Geschwindigkeit 3"):
         geschw = 3
     help = verbindungsTest.geschwStufe(geschw)
     write(str(help))
+
 
 frame = Frame(main)
 frame.grid(row=2, column=3)
@@ -52,28 +55,31 @@ scrollbar.pack(side=RIGHT, fill="y")
 frame.count = 1
 frame.configure(background='black')
 
+
 def write(txt):
     output.insert(END, str(txt + "\n"))
 
 
-
-selectedHoehe = StringVar()
+selectedHoehe = StringVar(main)
 selectedHoehe.set("Höhe 1")
-selectedGeschw = StringVar()
+selectedGeschw = StringVar(main)
 selectedGeschw.set("Geschwindigkeit 1")
 
 dropgeschw = OptionMenu(main, selectedGeschw, "Geschwindigkeit 1", "Geschwindigkeit 2", "Geschwindigkeit 3")
 dropgeschw.grid(row=1, column=2)
+dropgeschw.config(padx=20)
 drophoehe = OptionMenu(main, selectedHoehe, "Höhe 1", "Höhe 2", "Höhe 3")
 drophoehe.grid(row=1, column=1)
 
-hoeheeinstellen = Button(main, text="Höhe übernehmen", pady=20, padx=20, command=lambda:threading.Thread(target=hoeheEinstellen).start())
+hoeheeinstellen = Button(main, text="Höhe übernehmen", pady=20, padx=20,
+                         command=lambda: threading.Thread(target=hoeheEinstellen).start())
 hoeheeinstellen.grid(row=2, column=1)
-geschweinstellen = Button(main, text="Geschwindigkeit übernehmen", pady=20, padx=20, command=lambda:threading.Thread(target=geschwEinstellen).start())
+geschweinstellen = Button(main, text="Geschwindigkeit übernehmen", pady=20, padx=20,
+                          command=lambda: threading.Thread(target=geschwEinstellen).start())
 geschweinstellen.grid(row=2, column=2)
 
 write("Test")
-#test = Label(main, text="Test", padx=40, pady=40, width=20)
-#test.grid(row=1, column=4)
+# test = Label(main, text="Test", padx=40, pady=40, width=20)
+# test.grid(row=1, column=4)
 
 main.mainloop()
