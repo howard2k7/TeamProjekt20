e7b002a6f33276f06aaa237ea4284ddbbe126be8
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2020-12-11 10:07:42 +0100

    Layout

--- COM/main_gui.py
+++ COM/main_gui.py
@@ -5,23 +5,23 @@ import functools
 import operator
 
 main = Tk()
-main.geometry("1100x800")
+main.geometry("950x500")
 main.config(bg="grey")
 main.title("Main Interface")
 main.resizable(False, False)
 
 
-def convertString(tup):
+def convertString(string):
     helpstr = ""
-    for i in range(len(tup)):
-        if tup[i] != "'" and tup[i] != "(" and tup[i] != ")" and tup[i] != ",":
-            helpstr = helpstr + tup[i]
+    for i in range(len(string)):
+        if string[i] != "'" and string[i] != "(" and string[i] != ")" and string[i] != ",":
+            helpstr = helpstr + string[i]
     return helpstr
 
 
 def hoeheEinstellen():
     hoehe = 0
-    write(selectedHoehe.get())
+    write("Befehl: " + selectedHoehe.get())
     if (selectedHoehe.get() == "Höhe 1"):
         hoehe = 1
     elif (selectedHoehe.get() == "Höhe 2"):
@@ -34,7 +34,7 @@ def hoeheEinstellen():
 
 def geschwEinstellen():
     geschw = 0
-    write(selectedGeschw.get())
+    write("Befehl: " + selectedGeschw.get())
     if (selectedGeschw.get() == "Geschwindigkeit 1"):
         geschw = 1
     elif (selectedGeschw.get() == "Geschwindigkeit 2"):
@@ -44,18 +44,28 @@ def geschwEinstellen():
     help = verbindungsTest.geschwStufe(geschw)
     write(str(help))
 
+frameTop = Frame(main, padx=50, pady=50)
+frameTop.grid(row=0, column=0)
+frameTopRight = Frame(main, background='black', padx=50, pady=50)
+frameTopRight.grid(row=0, column=1)
+frameBot = Frame(main, background='black', padx=50, pady=50)
+frameBot.grid(row=1, column=0)
+frameBotRight = Frame(main, padx=50, pady=50)
+frameBotRight.grid(row=1, column=1)
 
-frame = Frame(main)
-frame.grid(row=2, column=3)
 
-output = Text(frame, width=50, height=15, background='black', fg='white')
+frameTerminal = Frame(frameBotRight)
+frameTerminal.grid(row=2, column=3)
+
+
+output = Text(frameTerminal, width=50, height=15, background='black', fg='white')
 output.pack(side=LEFT)
 
-scrollbar = Scrollbar(frame, orient="vertical", command=output.yview)
+scrollbar = Scrollbar(frameTerminal, orient="vertical", command=output.yview)
 scrollbar.pack(side=RIGHT, fill="y")
 
-frame.count = 1
-frame.configure(background='black')
+frameTerminal.count = 1
+frameTerminal.configure(background='black')
 
 
 def write(txt):
@@ -67,20 +77,20 @@ selectedHoehe.set("Höhe 1")
 selectedGeschw = StringVar(main)
 selectedGeschw.set("Geschwindigkeit 1")
 
-dropgeschw = OptionMenu(main, selectedGeschw, "Geschwindigkeit 1", "Geschwindigkeit 2", "Geschwindigkeit 3")
+dropgeschw = OptionMenu(frameTop, selectedGeschw, "Geschwindigkeit 1", "Geschwindigkeit 2", "Geschwindigkeit 3")
 dropgeschw.grid(row=1, column=2)
 dropgeschw.config(padx=20)
-drophoehe = OptionMenu(main, selectedHoehe, "Höhe 1", "Höhe 2", "Höhe 3")
+drophoehe = OptionMenu(frameTop, selectedHoehe, "Höhe 1", "Höhe 2", "Höhe 3")
 drophoehe.grid(row=1, column=1)
 
-hoeheeinstellen = Button(main, text="Höhe übernehmen", pady=20, padx=20,
+hoeheeinstellen = Button(frameTop, text="Höhe übernehmen", pady=20, padx=20,
                          command=lambda: threading.Thread(target=hoeheEinstellen).start())
 hoeheeinstellen.grid(row=2, column=1)
-geschweinstellen = Button(main, text="Geschwindigkeit übernehmen", pady=20, padx=20,
+geschweinstellen = Button(frameTop, text="Geschwindigkeit übernehmen", pady=20, padx=20,
                           command=lambda: threading.Thread(target=geschwEinstellen).start())
 geschweinstellen.grid(row=2, column=2)
 
-write("Test")
+write("Betriebsbereit !")
 # test = Label(main, text="Test", padx=40, pady=40, width=20)
 # test.grid(row=1, column=4)
 
