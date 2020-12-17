99ab2b16800da9008e84abf7dabc83bb8bc3db3b
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2020-12-17 12:32:27 +0100

    Gui in class

--- COM/main_gui.py
+++ COM/main_gui.py
@@ -2,98 +2,102 @@ from tkinter import *
 import threading
 import verbindungsTest
 
-main = Tk()
-main.geometry("1100x500")
-main.config(bg="grey")
-main.title("Main Interface")
-main.resizable(False, False)
+
+class mainGui:
+    def __init__(self, master):
+        self.master = master
+        master.geometry("1100x500")
+        master.config(bg="grey")
+        master.title("Main Interface")
+        master.resizable(False, False)
+
+        #building frame grid
+        self.frameTop = Frame(master, padx=50, pady=50)
+        self.frameTop.grid(row=0, column=0)
+        self.frameTopRight = Frame(master, background='black', padx=50, pady=50)
+        self.frameTopRight.grid(row=0, column=1)
+        self.frameBot = Frame(master, background='black', padx=50, pady=50)
+        self.frameBot.grid(row=1, column=0)
+        self.frameBotRight = Frame(master, padx=50, pady=50)
+        self.frameBotRight.grid(row=1, column=1)
+
+        # frame for terminal
+        self.frameTerminal = Frame(self.frameBotRight)
+        self.frameTerminal.grid(row=2, column=3)
+        self.frameTerminal.configure(background='black')
+
+        # create scrollbar
+        self.scrollbar = Scrollbar(self.frameTerminal)
+        self.scrollbar.pack(side=RIGHT, fill="y")
+
+        # create output
+        self.output = Listbox(self.frameTerminal, yscrollcommand=self.scrollbar.set , width=50, height=15, background='black', fg='white')
+        self.output.pack(side=LEFT)
+        self.scrollbar.config(command=self.output.yview)
+
+        # initial set for variables
+        self.selectedHeight = StringVar(main)
+        self.selectedHeight.set("Höhe 1")
+        self.selectedPace = StringVar(main)
+        self.selectedPace.set("Geschwindigkeit 1")
+
+        # creating drop menus
+        self.dropPace = OptionMenu(self.frameTop, self.selectedPace, "Geschwindigkeit 1", "Geschwindigkeit 2", "Geschwindigkeit 3")
+        self.dropPace.grid(row=1, column=2)
+        self.dropPace.config(padx=20)
+        self.dropHeight = OptionMenu(self.frameTop, self.selectedHeight, "Höhe 1", "Höhe 2", "Höhe 3")
+        self.dropHeight.grid(row=1, column=1)
+
+        # creating Buttons
+        self.heightSelectedButton = Button(self.frameTop, text="Höhe übernehmen", pady=20, padx=20, command=lambda: threading.Thread(target=self.heightSelect).start())
+        self.heightSelectedButton.grid(row=2, column=1)
+        self.paceSelectedButton = Button(self.frameTop, text="Geschwindigkeit übernehmen", pady=20, padx=20, command=lambda: threading.Thread(target=self.paceSelect).start())
+        self.paceSelectedButton.grid(row=2, column=2)
+
+        # initial terminal message
+        self.write("Betriebsbereit !")
 
 
-def convertString(string):
-    helpStr = ""
-    for i in range(len(string)):
-        if string[i] != "'" and string[i] != "(" and string[i] != ")" and string[i] != ",":
-            helpStr = helpStr + string[i]
-    return helpStr
+    def convertString(self, string):
+        helpStr = ""
+        for i in range(len(string)):
+            if string[i] != "'" and string[i] != "(" and string[i] != ")" and string[i] != ",":
+                helpStr = helpStr + string[i]
+        return helpStr
 
 
-def heightSelect():
-    height = 0
-    write("Befehl: " + selectedHeight.get())
-    if selectedHeight.get() == "Höhe 1":
-        height = 1
-    elif selectedHeight.get() == "Höhe 2":
-        height = 2
-    elif selectedHeight.get() == "Höhe 3":
-        height = 3
-        # call heightSelect method with height
-    help = verbindungsTest.hoeheStufe(height)
-    write(str(help))
+    def heightSelect(self):
+        height = 0
+        self.write("Befehl: " + self.selectedHeight.get())
+        if self.selectedHeight.get() == "Höhe 1":
+            height = 1
+        elif self.selectedHeight.get() == "Höhe 2":
+            height = 2
+        elif self.selectedHeight.get() == "Höhe 3":
+            height = 3
+            # call heightSelect method with height
+        help = verbindungsTest.hoeheStufe(height)
+        self.write(str(help))
 
 
-def paceSelect():
-    pace = 0
-    write("Befehl: " + selectedPace.get())
-    if selectedPace.get() == "Geschwindigkeit 1":
-        pace = 1
-    elif selectedPace.get() == "Geschwindigkeit 2":
-        pace = 2
-    elif selectedPace.get() == "Geschwindigkeit 3":
-        pace = 3
+    def paceSelect(self):
+        pace = 0
+        self.write("Befehl: " + self.selectedPace.get())
+        if self.selectedPace.get() == "Geschwindigkeit 1":
+            pace = 1
+        elif self.selectedPace.get() == "Geschwindigkeit 2":
+            pace = 2
+        elif self.selectedPace.get() == "Geschwindigkeit 3":
+            pace = 3
         # call paceSelect method with pace
-    help = verbindungsTest.geschwStufe(pace)
-    write(str(help))
+        help = verbindungsTest.geschwStufe(pace)
+        self.write(str(help))
 
-# building frame grid
-frameTop = Frame(main, padx=50, pady=50)
-frameTop.grid(row=0, column=0)
-frameTopRight = Frame(main, background='black', padx=50, pady=50)
-frameTopRight.grid(row=0, column=1)
-frameBot = Frame(main, background='black', padx=50, pady=50)
-frameBot.grid(row=1, column=0)
-frameBotRight = Frame(main, padx=50, pady=50)
-frameBotRight.grid(row=1, column=1)
-
-# frame for terminal
-frameTerminal = Frame(frameBotRight)
-frameTerminal.grid(row=2, column=3)
-frameTerminal.configure(background='black')
-
-# create scrollbar
-scrollbar = Scrollbar(frameTerminal)
-scrollbar.pack(side=RIGHT, fill="y")
-
-# create output
-output = Listbox(frameTerminal, yscrollcommand=scrollbar.set , width=50, height=15, background='black', fg='white')
-output.pack(side=LEFT)
-scrollbar.config(command=output.yview)
+    def write(self, txt):
+        self.output.insert(END, str(self.convertString(txt) + "\n"))
 
 
-def write(txt):
-    output.insert(END, str(convertString(txt) + "\n"))
-
-# initial set for variables
-selectedHeight = StringVar(main)
-selectedHeight.set("Höhe 1")
-selectedPace = StringVar(main)
-selectedPace.set("Geschwindigkeit 1")
-
-# creating drop menus
-dropPace = OptionMenu(frameTop, selectedPace, "Geschwindigkeit 1", "Geschwindigkeit 2", "Geschwindigkeit 3")
-dropPace.grid(row=1, column=2)
-dropPace.config(padx=20)
-dropHeight = OptionMenu(frameTop, selectedHeight, "Höhe 1", "Höhe 2", "Höhe 3")
-dropHeight.grid(row=1, column=1)
-
-# creating Buttons
-heightSelectedButton = Button(frameTop, text="Höhe übernehmen", pady=20, padx=20,
-                              command=lambda: threading.Thread(target=heightSelect).start())
-heightSelectedButton.grid(row=2, column=1)
-paceSelectedButton = Button(frameTop, text="Geschwindigkeit übernehmen", pady=20, padx=20,
-                            command=lambda: threading.Thread(target=paceSelect).start())
-paceSelectedButton.grid(row=2, column=2)
-
-# initial terminal message
-write("Betriebsbereit !")
-
-main.mainloop()
+if __name__ == "__main__":
+    main = Tk()
+    my_maingui = mainGui(main)
+    main.mainloop()
