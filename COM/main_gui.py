37683a8e94ca4673bfb98b69ead8736affdd4452
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2020-12-17 15:05:36 +0100

    Konsole 2

--- COM/main_gui.py
+++ COM/main_gui.py
@@ -8,7 +8,7 @@ import re
 class mainGui:
     def __init__(self, master):
         self.master = master
-        master.geometry("1300x500")
+        master.geometry("1500x700")
         master.config(bg="grey")
         master.title("Main Interface")
         master.resizable(False, False)
@@ -18,7 +18,7 @@ class mainGui:
         self.frameTop.grid(row=0, column=0)
         self.frameTopRight = Frame(master, padx=50, pady=50)
         self.frameTopRight.grid(row=0, column=1)
-        self.frameBot = Frame(master, background='black', padx=50, pady=50)
+        self.frameBot = Frame(master, padx=50, pady=50)
         self.frameBot.grid(row=1, column=0)
         self.frameBotRight = Frame(master, padx=50, pady=50)
         self.frameBotRight.grid(row=1, column=1)
@@ -28,14 +28,29 @@ class mainGui:
         self.frameTerminal.grid(row=2, column=3)
         self.frameTerminal.configure(background='black')
 
+        # frame for terminal2
+        self.frameTerminal2 = Frame(self.frameBot)
+        self.frameTerminal2.grid(row=0, column=0)
+        self.frameTerminal2.configure(background='black')
+
         # create scrollbar
         self.scrollbar = Scrollbar(self.frameTerminal)
         self.scrollbar.pack(side=RIGHT, fill="y")
+
+        # create scrollbar2
+        self.scrollbar2 = Scrollbar(self.frameTerminal2)
+        self.scrollbar2.pack(side=RIGHT, fill="y")
 
         # create output
         self.output = Listbox(self.frameTerminal, yscrollcommand=self.scrollbar.set , width=50, height=15, background='black', fg='white')
         self.output.pack(side=LEFT)
         self.scrollbar.config(command=self.output.yview)
+
+        # create output
+        self.output2 = Listbox(self.frameTerminal2, yscrollcommand=self.scrollbar2.set, width=50, height=15,
+                              background='black', fg='white')
+        self.output2.pack(side=LEFT)
+        self.scrollbar2.config(command=self.output2.yview)
 
         # initial set for variables
         self.selectedHeight = StringVar(main)
@@ -58,6 +73,7 @@ class mainGui:
 
         # initial terminal message
         self.write("Betriebsbereit !")
+        self.write2("Betriebsbereit !")
 
 
         # create labels
@@ -157,8 +173,12 @@ class mainGui:
     def write(self, txt):
         self.output.insert(END, str(self.convertString(txt) + "\n"))
 
+    def write2(self, txt):
+        self.output2.insert(END, str(txt + "\n"))
+
 
 if __name__ == "__main__":
     main = Tk()
     my_maingui = mainGui(main)
     main.mainloop()
+
